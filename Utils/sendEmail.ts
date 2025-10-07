e0d0e5d09d7d8659e5bac8a229bea9ef6453eb2f
tungpy78 <truonganhtung230904@gmail.com>
2025-10-08 01:16:44 +0700

    test redis new

--- Utils/sendEmail.ts
+++ Utils/sendEmail.ts
@@ -6,11 +6,15 @@ dotenv.config();
 
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail', // hoặc smtp server khác
+  host: 'smtp.gmail.com',
+  port: 465,          // hoặc 587
+  secure: true,       // nếu dùng 465 -> true; nếu 587 -> false và thêm requireTLS
   auth: {
     user: process.env.MAIL_USER,      // email bạn dùng gửi mail
     pass: process.env.MAIL_PASSWORD,  // mật khẩu ứng dụng (app password)
   },
+  // tuỳ chọn: tăng timeout nếu mạng chậm
+  connectionTimeout: 15000,
 });
 
 export const sendMail = async (to: string, subject: string, html: string) => {
